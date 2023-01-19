1045a7fa6685265abea6843cea3d2b73f152634a
mehriddinl8 <mehriddinsaidovl8@gmail.com>
2023-01-19 23:56:35 +0500

    add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import errorimg from './components/img/github.png'
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path='*' element={<div className='m bg-[#a7e9e9] h-[100vh]'>
+        <h1 style={{ fontSize: '3em', textAlign: 'center' }}>ERROR 404</h1>
+        <img className='m-auto my-[5vh]' src={errorimg} alt="" /></div>} />
+    </Routes>
+    </div >
   );
 }
 
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import { Container } from '@mui/system'
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+function Main() {
+    return (
+        <div>
+            <Container>
+                <div>
+                    <div>
+                    <p>8 499 430-01-64</p>
+                    <p>info@shantui.com.ru</p>
+                    </div>
+                    <div>
+                        <FacebookIcon/>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Main
