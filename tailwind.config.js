28978a5be9fa4f84cb4bae1bed2f038a30ba9885
mehriddinl8 <mehriddinsaidovl8@gmail.com>
2023-02-03 18:53:55 +0500

    edit tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       phone2: {max: '370px'},
       tablet1: {max: '664px'},
       tablet2: {max: '768px'},
+      lp: {max: '900px'},
       laptop1: {max: '1072px'},
       laptop2: {max: '1280px'},
       desktop: {max: '1536px'},
